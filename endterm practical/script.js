976c8e19ff9877cc8349a7103cc07cf25722e907
chauhan508 <dushyantchauhan9953@gmail.com>
2026-06-25 09:52:42 +0530

    student result calculator

---
+++ endterm practical/script.js
@@ -0,0 +1,34 @@
+function calculateResult() {
+
+    let s1 = Number(document.getElementById("sub1").value);
+    let s2 = Number(document.getElementById("sub2").value);
+    let s3 = Number(document.getElementById("sub3").value);
+    let s4 = Number(document.getElementById("sub4").value);
+    let s5 = Number(document.getElementById("sub5").value);
+
+    let total = s1 + s2 + s3 + s4 + s5;
+    let percentage = total / 5;
+
+    let grade;
+
+    if (percentage >= 90)
+        grade = "A+";
+    else if (percentage >= 80)
+        grade = "A";
+    else if (percentage >= 70)
+        grade = "B";
+    else if (percentage >= 60)
+        grade = "C";
+    else if (percentage >= 40)
+        grade = "D";
+    else
+        grade = "F";
+
+    let status = (percentage >= 40) ? "PASS" : "FAIL";
+
+    document.getElementById("result").innerHTML =
+        `Total Marks: ${total}<br>
+         Percentage: ${percentage.toFixed(2)}%<br>
+         Grade: ${grade}<br>
+         Status: ${status}`;
+}
